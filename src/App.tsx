64260828c847df4cada38a049d3b180baccdc265
Chingis <chingisdev@gmail.com>
2022-07-12 15:04:33 +0300

    basic dumb components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import Logo from "./components/Logo";
+import Footer from "./components/Footer";
+import styles from './App.module.css';
 
 function App() {
-
+  return (
+      <div className={styles.app}>
+        <Logo />
+        <main className={styles.content} />
+        <Footer />
+      </div>
+  )
 }
 
 export default App;
