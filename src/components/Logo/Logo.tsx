88a57389c1660d1b0c27558db15eaa24940ca243
Chingis <chingisdev@gmail.com>
2022-07-12 14:52:46 +0300

    dumb components

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import styles from './Logo.module.css';
+
+export const Logo = () => {
+    return (
+        <h1 className={styles.logo}>
+            <span role="img" aria-label="jesus cries">&#271D</span>
+            <span role="img" aria-label="music notes">&#127926</span>
+            <span role="img" aria-label="music keyboard">&#127929</span>
+        </h1>
+    )
+}
